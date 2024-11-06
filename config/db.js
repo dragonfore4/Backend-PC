f6044c6ccde2a0a73bd45b8a1d265f8a859d3aca
kemkai <kemkaikub@gmail.com>
2024-11-06 20:25:15 +0700

    change config db

--- config/db.js
+++ config/db.js
@@ -9,12 +9,13 @@ require("dotenv").config({path: ".env.local"});
 //     database : "carbon_market"
 // })
 
-
+// mysql://root:RORUgYrGpNfrluALtBEkjJMVgFWjJSTo@junction.proxy.rlwy.net:25591/railway
 const pool = mysql.createPool({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    host : process.env.DB_HOST || 'localhost',
+    user : process.env.DB_USER || 'root',
+    password : process.env.DB_PASSWORD || '1234',
+    database : process.env.DB_NAME || 'carbon_credit_db',
+    port: process.env.DB_PORT || 3306
 })
 
 
