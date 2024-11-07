0239cec6ca871bf125d5cfa0b35b79f67c4a98e8
kemkai <kemkaikub@gmail.com>
2024-11-07 17:53:20 +0700

    Resolve merge conflicts

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
-const { register , login , user, getUserIdByUsername } = require("../Controllers/auth");
+const { register , login , user, getUserIdByUsername, getToken, decodeToken, deleteToken } = require("../Controllers/auth");
 const { auth } = require("../Middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/user", auth, user);
 router.post("/getUserIdByUsername", auth, getUserIdByUsername)
+router.get("/getToken", getToken);
+router.get("/decodeToken", decodeToken);
+router.get("/deleteToken", deleteToken);
 
 
 module.exports = router;
